e20c60019fddbf81109c9a8a6c8fd49b558e352f
lukw4l <lw.lukas.walter@gmail.com>
2021-06-24 23:53:51 +0200

    Create a basic app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MainComponent } from './main/main.component';
+import { ActionComponent } from './action/action.component';
+import { RetroComponent } from './retro/retro.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [ AppComponent, HelloComponent, MainComponent, ActionComponent, RetroComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
