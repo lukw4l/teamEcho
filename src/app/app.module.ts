e28c06773924cedc5d479025c739bdb6c7985459
michaelrex <michael.rex@talanx.com>
2021-06-25 09:17:12 +0200

    bugfixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { appRoutingModule } from './app.routing';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, appRoutingModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     HelloComponent,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 import { AuthGuard } from './_helpers';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: AppComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
